a33a18791db9be617b2ab1da69974ea117a67a84
FlipeFrontDev <felipe.saint@gmail.com>
2021-12-31 01:02:08 -0300

    Update Readme

---
+++ 2-OO/bytebank/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Conta conta1 = new Conta(123,456);
+        Conta conta2 = new Conta(789,789);
+    }
+}
